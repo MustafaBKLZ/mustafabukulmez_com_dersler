39812e8e22add407274979ed8b5f9d0d511d8b42
Mustafa <mustafabukulmez3446@gmail.com>
2019-09-05 15:58:27 +0300

    xml ders serisi dersleri ekleniyor.

--- mustafabukulmez_com_dersler/_023_XML_Islemleri/XML_Diger_Islemler.Designer.cs
+++ mustafabukulmez_com_dersler/_023_XML_Islemleri/XML_Diger_Islemler.Designer.cs
@@ -29,6 +29,20 @@
         private void InitializeComponent()
         {
             this.btn_dataset_ile_olustur = new System.Windows.Forms.Button();
+            this.btn_xml_den_dataset = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_xml_de_ara = new System.Windows.Forms.Button();
+            this.txt_aranacak_metin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_sonuc = new System.Windows.Forms.Label();
+            this.btn_xml_filtre = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ucret_filtre = new System.Windows.Forms.TextBox();
+            this.btn_xml_den_excel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_dataset_ile_olustur
@@ -41,22 +55,157 @@
             this.btn_dataset_ile_olustur.UseVisualStyleBackColor = true;
             this.btn_dataset_ile_olustur.Click += new System.EventHandler(this.Btn_dataset_ile_olustur_Click);
             // 
+            // btn_xml_den_dataset
+            // 
+            this.btn_xml_den_dataset.Location = new System.Drawing.Point(12, 88);
+            this.btn_xml_den_dataset.Name = "btn_xml_den_dataset";
+            this.btn_xml_den_dataset.Size = new System.Drawing.Size(247, 70);
+            this.btn_xml_den_dataset.TabIndex = 1;
+            this.btn_xml_den_dataset.Text = "XML ile DataSet Dosyası Oluştur";
+            this.btn_xml_den_dataset.UseVisualStyleBackColor = true;
+            this.btn_xml_den_dataset.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(265, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(675, 146);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btn_xml_de_ara
+            // 
+            this.btn_xml_de_ara.Location = new System.Drawing.Point(12, 164);
+            this.btn_xml_de_ara.Name = "btn_xml_de_ara";
+            this.btn_xml_de_ara.Size = new System.Drawing.Size(247, 70);
+            this.btn_xml_de_ara.TabIndex = 3;
+            this.btn_xml_de_ara.Text = "XML Dosyada Arama Yapmak";
+            this.btn_xml_de_ara.UseVisualStyleBackColor = true;
+            this.btn_xml_de_ara.Click += new System.EventHandler(this.Btn_xml_de_ara_Click);
+            // 
+            // txt_aranacak_metin
+            // 
+            this.txt_aranacak_metin.Location = new System.Drawing.Point(353, 189);
+            this.txt_aranacak_metin.Name = "txt_aranacak_metin";
+            this.txt_aranacak_metin.Size = new System.Drawing.Size(247, 20);
+            this.txt_aranacak_metin.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Aranacak Metin";
+            // 
+            // lbl_sonuc
+            // 
+            this.lbl_sonuc.AutoSize = true;
+            this.lbl_sonuc.Location = new System.Drawing.Point(606, 193);
+            this.lbl_sonuc.Name = "lbl_sonuc";
+            this.lbl_sonuc.Size = new System.Drawing.Size(47, 13);
+            this.lbl_sonuc.TabIndex = 6;
+            this.lbl_sonuc.Text = "Sonuç : ";
+            // 
+            // btn_xml_filtre
+            // 
+            this.btn_xml_filtre.Location = new System.Drawing.Point(12, 240);
+            this.btn_xml_filtre.Name = "btn_xml_filtre";
+            this.btn_xml_filtre.Size = new System.Drawing.Size(247, 70);
+            this.btn_xml_filtre.TabIndex = 7;
+            this.btn_xml_filtre.Text = "XML Dosyada Filtre Vermek";
+            this.btn_xml_filtre.UseVisualStyleBackColor = true;
+            this.btn_xml_filtre.Click += new System.EventHandler(this.Btn_xml_filtre_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(265, 262);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(675, 146);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Proje Ücreti Filtre";
+            // 
+            // txt_ucret_filtre
+            // 
+            this.txt_ucret_filtre.Location = new System.Drawing.Point(350, 236);
+            this.txt_ucret_filtre.Name = "txt_ucret_filtre";
+            this.txt_ucret_filtre.Size = new System.Drawing.Size(247, 20);
+            this.txt_ucret_filtre.TabIndex = 9;
+            // 
+            // btn_xml_den_excel
+            // 
+            this.btn_xml_den_excel.Enabled = false;
+            this.btn_xml_den_excel.Location = new System.Drawing.Point(12, 423);
+            this.btn_xml_den_excel.Name = "btn_xml_den_excel";
+            this.btn_xml_den_excel.Size = new System.Drawing.Size(247, 70);
+            this.btn_xml_den_excel.TabIndex = 11;
+            this.btn_xml_den_excel.Text = "XML den EXCEL Oluşturma";
+            this.btn_xml_den_excel.UseVisualStyleBackColor = true;
+            this.btn_xml_den_excel.Click += new System.EventHandler(this.Btn_xml_den_excel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 499);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 70);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "EXCEL den XML Oluşturma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
             // XML_Diger_Islemler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1149, 538);
+            this.ClientSize = new System.Drawing.Size(1149, 684);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_xml_den_excel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_ucret_filtre);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btn_xml_filtre);
+            this.Controls.Add(this.lbl_sonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_aranacak_metin);
+            this.Controls.Add(this.btn_xml_de_ara);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_xml_den_dataset);
             this.Controls.Add(this.btn_dataset_ile_olustur);
             this.Name = "XML_Diger_Islemler";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "XML_Diger_Islemler";
             this.Load += new System.EventHandler(this.XML_Diger_Islemler_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_dataset_ile_olustur;
+        private System.Windows.Forms.Button btn_xml_den_dataset;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_xml_de_ara;
+        private System.Windows.Forms.TextBox txt_aranacak_metin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_sonuc;
+        private System.Windows.Forms.Button btn_xml_filtre;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ucret_filtre;
+        private System.Windows.Forms.Button btn_xml_den_excel;
+        private System.Windows.Forms.Button button1;
     }
 }
--- mustafabukulmez_com_dersler/_023_XML_Islemleri/XML_Diger_Islemler.cs
+++ mustafabukulmez_com_dersler/_023_XML_Islemleri/XML_Diger_Islemler.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace mustafabukulmez_com_dersler._023_XML_Islemleri
 {
@@ -56,7 +58,142 @@ namespace mustafabukulmez_com_dersler._023_XML_Islemleri
             dr["proje_Ucreti"] = pPrice;
             dt.Rows.Add(dr);
         }
+
         #endregion
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(_000_Classlar.Global.AppPath + "\\_023_XML_Islemleri\\XML\\Proje.xml", new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFile);
+            dataGridView1.DataSource = ds.Tables[0];
+
+
+        }
+
+        private void Btn_xml_de_ara_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(_000_Classlar.Global.AppPath + "\\_023_XML_Islemleri\\XML\\Proje.xml", new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            DataView dv;
+            ds.ReadXml(xmlFile);
+
+            dv = new DataView(ds.Tables[0]);
+            dv.Sort = "Proje_Adı";
+            int index = dv.Find(txt_aranacak_metin.Text);
+
+            if (index == -1)
+            {
+                lbl_sonuc.Text = "Sonuç: Bulunamadı.";
+            }
+            else
+            {
+                lbl_sonuc.Text = "Sonuç: Adı: " + dv[index]["Proje_Adı"].ToString() + " || Fiyatı:" + dv[index]["proje_Ucreti"].ToString();
+            }
+        }
+
+        private void Btn_xml_filtre_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(_000_Classlar.Global.AppPath + "\\_023_XML_Islemleri\\XML\\Proje.xml", new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            DataView dv;
+            ds.ReadXml(xmlFile);
+            dv = new DataView(ds.Tables[0], "proje_Ucreti > = " + txt_ucret_filtre.Text + "", "Proje_Adı", DataViewRowState.CurrentRows);
+            dv.ToTable().WriteXml("Result.xml");
+
+            dataGridView2.DataSource = dv.ToTable();
+
+        }
+
+        private void Btn_xml_den_excel_Click(object sender, EventArgs e)
+        {
+            //Microsoft.Office.Interop.Excel.Application xlApp;
+            //Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            //Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            //object misValue = System.Reflection.Missing.Value;
+
+            //DataSet ds = new DataSet();
+            //XmlReader xmlFile;
+            //int i = 0;
+            //int j = 0;
+
+            //// xlApp = new Microsoft.Office.Interop.Excel.ApplicationClass(); 
+            //// Burada bir hata var ama çözemedim
+
+
+            ////xlWorkBook = xlApp.Workbooks.Add(misValue);
+            //xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            //xmlFile = XmlReader.Create("Product.xml", new XmlReaderSettings());
+            //ds.ReadXml(xmlFile);
+
+            //for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            //{
+            //    for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+            //    {
+            //        xlWorkSheet.Cells[i + 1, j + 1] = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+            //    }
+            //}
+
+            //xlWorkBook.SaveAs("Specify path here\\xml2excel.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            //xlWorkBook.Close(true, misValue, misValue);
+            //xlApp.Quit();
+
+            //releaseObject(xlApp);
+            //releaseObject(xlWorkBook);
+            //releaseObject(xlWorkSheet);
+
+            //MessageBox.Show("Done .. ");
+        }
+        //private void releaseObject(object obj)
+        //{
+        //    try
+        //    {
+        //        System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+        //        obj = null;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        obj = null;
+        //    }
+        //    finally
+        //    {
+        //        GC.Collect();
+        //    }
+        //} 
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                System.Data.OleDb.OleDbConnection MyConnection;
+                System.Data.DataSet ds;
+                System.Data.OleDb.OleDbDataAdapter MyCommand;
+                MyConnection = new System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + _000_Classlar.Global.AppPath + "\\_023_XML_Islemleri\\XML\\Test.xls" + "';Extended Properties=Excel 8.0;");
+                MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Sayfa1$]", MyConnection);
+                MyCommand.TableMappings.Add("Table", "Test");
+                ds = new System.Data.DataSet();
+                MyCommand.Fill(ds);
+                MyConnection.Close();
+                ds.WriteXml(_000_Classlar.Global.AppPath + "\\_023_XML_Islemleri\\XML\\Test.xml");
+                dataGridView2.DataSource = ds.Tables[0];
+
+
+             
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
 
 
     }
