43a3c390accad85793d694eb54c5fe32c804d590
Mustafa <mustafabukulmez3446@gmail.com>
2022-03-28 13:44:26 +0300

    DERS 45 Yayınlandı & Birkaç Düzeltme

--- mustafabukulmez_com_dersler/_045_SHA384_Sifreleme/Form1.cs
+++ mustafabukulmez_com_dersler/_045_SHA384_Sifreleme/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 
-namespace mustafabukulmez_com_dersler._044_SHA256_Sifreleme
+namespace mustafabukulmez_com_dersler._045_SHA384_Sifreleme
 {
     public partial class Form1 : Form
     {
@@ -21,17 +21,17 @@ namespace mustafabukulmez_com_dersler._044_SHA256_Sifreleme
         private void Form1_Load(object sender, EventArgs e)
         {
             string metin = "mustafabükülmez";
-            label1.Text = metin + " metnin SHA256 hash çıktısı - ";
-            textBox1.Text = SHA256Hash(metin);
+            label1.Text = metin + " metnin SHA384 hash çıktısı - ";
+            textBox1.Text = SHA384Hash(metin);
         }
 
-        string SHA256Hash(string text)
+        string SHA384Hash(string text)
         {
             string source = text;
-            using (SHA256 sha256Hash = SHA256.Create())
+            using (SHA384 sha384Hash = SHA384.Create())
             {
                 byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
-                byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+                byte[] hashBytes = sha384Hash.ComputeHash(sourceBytes);
                 string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
                 return hash;
             }
